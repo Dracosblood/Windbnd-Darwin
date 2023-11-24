cbebf3b1487746ea238f6b8b2d823448d86662ee
Darwin J. Tovar <135628439+dracosblood@users.noreply.github.com>
2023-11-24 10:31:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/MIniProyectoDarwin/'
+  base : 'https://dracosblood.github.io/Windbnd-Darwin/'
 })
